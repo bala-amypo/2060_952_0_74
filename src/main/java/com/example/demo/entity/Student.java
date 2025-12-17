c6655172df264fa4d9e098f1e863bbe37d9e69ee
bala-amypo <bala@amypo.in>
2025-12-17 04:57:48 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -6,6 +6,7 @@ import jakarta.parsistence.*;
 @Entity
 public class Student{
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String dept;

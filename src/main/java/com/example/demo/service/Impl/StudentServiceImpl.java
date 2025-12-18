6c43c68b04e7792d209a3ce1464918b9002d8b93
bala-amypo <bala@amypo.in>
2025-12-18 06:28:20 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/StudentServiceImpl.java
@@ -39,7 +39,7 @@ public class StudentServiceImpl implements StudentService {
             stdrepo.deleteById(id);
             return "Student Deleted Successfully";
         }else{
-            return
+            return "Student "
         }
     }
 }
